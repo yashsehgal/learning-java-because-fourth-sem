a408683f5119ba3de31c85db6ef923645219c4be
Yash Sehgal <yashsehgal.work@gmail.com>
2022-01-18 08:32:57 +0000

    String Operations module added

---
+++ modules/StringOperations/StringOperations.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class StringOperations {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		// taking test input
+		String testInput;
+		System.out.println("Type a test string: ");
+		testInput = scan.nextLine();
+		System.out.println(">>> " + testInput);
+	}
+}
