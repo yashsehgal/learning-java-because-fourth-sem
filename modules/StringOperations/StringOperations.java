1a1a19c1c128c052370f0447a26db2a73f3566b4
Yash Sehgal <yashsehgal.work@gmail.com>
2022-01-18 09:02:06 +0000

    String Operations module added

--- modules/StringOperations/StringOperations.java
+++ modules/StringOperations/StringOperations.java
@@ -1,12 +1,31 @@
 import java.util.*;
 
 public class StringOperations {
+	public static String[] generateUsername(String usernameString[], boolean isUsernameStringValid) {
+		isUsernameStringValid=false;
+		if (usernameStringValid) {
+		       	isUsernameStringValid=false; 
+		} else {
+			isUsernameStringValid=true;
+		}
+		if (isUsernameStringValid) {
+			for (int count = 0; count <= usernameString.length; count++) {
+				if (usernameString[count] == usernameString[count].toUpperCase()) {
+					usernameString[count].toLowerCase();
+				}
+				if (usernameString[count] == (String)" " || usernameString[count] == (String)"\t") {
+					usernameString[count] = (String)"-";
+				} 
+			}
+		}
+		return (String[])usernameString;
+	}
 	public static void main(String args[]) {
 		Scanner scan = new Scanner(System.in);
 		// taking test input
-		String testInput;
-		System.out.println("Type a test string: ");
-		testInput = scan.nextLine();
-		System.out.println(">>> " + testInput);
+		String testInput = "Yash Sehgal is an amaZing Developer. A really cool DesigNER";
+		System.out.println("Test Input: " + testInput);
+		String generatedUsernameResult = generateUsername(testString, false);
+		System.out.println("Test results: " + generatedUsernameResult);
 	}
 }
